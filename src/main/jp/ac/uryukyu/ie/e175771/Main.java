73e0dd2832ef44d764cd8af2387d944d3c122b97
Asamatsu <e175771@ie.u-ryukyu.ac.jp>
2020-01-20 22:14:16 +0900

    盤面Fieldクラス生成

--- src/main/jp/ac/uryukyu/ie/e175771/Main.java
+++ src/main/jp/ac/uryukyu/ie/e175771/Main.java
@@ -1,5 +1,10 @@
 package jp.ac.uryukyu.ie.e175771;
 
 public class Main {
+    public static void main(String args[]){
 
+        Field field = new Field();
+
+        field.print_board();
+    }
 }
